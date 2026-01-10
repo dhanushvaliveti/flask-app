231d6f56e590a8c6a615c47834a61967b50e1e1c
VALIVETI DHANUSH VENKATA NAGARAJU <dhanushvaliveti356@gmail.com>
2026-01-10 12:49:00 +0530

    Complete Flask Step 1: GET and POST JSON APIs

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 app=Flask(__name__)
-@app.route("/status")
+@app.route("/")
 def hello():
-	return jsonify({"msg": "hello"})
-if __name__ == "__main__"
-    app.run(debug=True)
+    return jsonify({"hello":"i am dhanush"})
+@app.route("/status")
+def status():
+	return jsonify({"status": "i am okay"})
+@app.route("/echo",methods=["POST"])
+def echo():
+    data=request.get_json()
+    if data is None:
+        return jsonify({"error": "No JSON received"}), 400
+    return jsonify(data)
+@app.route("/sum",methods=["POST"])
+def sum():
+    data=request.get_json()
+    if data is None:
+        return jsonify({"errror":"no json recieved"}),400
+    if "a" not in data or "b" not in data:
+        return jsonify({"errror":"no json recieved"}),400
+    result=data["a"]*data["b"]
+    return jsonify({"result":result})
+if __name__ == "__main__":
+    app.run()
+
 
